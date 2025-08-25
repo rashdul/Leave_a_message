2d47a0d93dd7aff2aec0e2d6b65786839c9af75f
Rashed Aldulijan <rashdul@hotmail.com>
2025-08-25 12:45:36 -0400

    JSON file fixed

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=database.engine)
 
 
+# Allow your Netlify site to talk to your API
+origins = [
+    "https://rdulijan.com",  # replace with your actual Netlify domain
+    "http://localhost:5500",  # useful for local dev if you test from file:// or localhost
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] if you want to allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/leave_a_message")
 def leave_message(
